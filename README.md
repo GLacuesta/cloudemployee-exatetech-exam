b2894fda13a405e2698dceb2d0024f26cb6e0175
AbstractGL <lacuesta.gerry@gmail.com>
2018-01-03 20:50:49 +0800

    For exam

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cloudemployee-exatetech-exam
+
+See test.js
+
+I'm not sure if it'll meet your expectations but I tried my best to follow the instruction. I put to codes all the logic that I've thought during my assessment for the tasks.
+
+
+Task 1 has been tested by injecting codes to the browser's console.
+
+
+Task 2 hasn't been tested yet since it may need more codes for cosmetics such as icons and css implementations. Since it'll involve css and designing, I may not make it during the 4 hour period. I only put the logic during the timeframe, hope it helps.
---
+++ test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function() {
+
+//task 1
+function searchAndReplace(word){
+	var page = [document.body], pageObj;
+
+	while (pageObj = page.pop()) {
+		if (!pageObj.textContent.match(word)) continue;
+		for (let i = 0; i < pageObj.childNodes.length; ++i) {
+			switch (pageObj.childNodes[i].nodeType) {
+				case Node.ELEMENT_NODE :
+					page.push(pageObj.childNodes[i]);
+					break;
+				case Node.TEXT_NODE :
+					if (pageObj.childNodes[i].textContent.match(word)) {
+						pageObj.childNodes[i].textContent = pageObj.childNodes[i].textContent.replace(word, "asd ");
+					}
+					break;
+			}
+		}
+	}
+}
+
+
+//task 2
+
+//on document ready
+function bindToTextbox(){
+    var allInputs = document.getElementsByTagName('input');
+    for (i = 0; i < allInputs.length; i++) {
+        if (allInputs[i].type == 'text') {
+            // add other css implementation for green and red icons
+            allInputs[i].addEventListener('click', 'selectTextbox(this)');
+        }
+    }
+}
+
+
+var container = {}; // array for all textbox that will be clicked
+
+function selectTextbox(elem){
+    if(elem.tagName != "INPUT" && elem.type != "text") return;
+
+    //additional css implementation if green icon switch to red and vice versa
+
+    return (container.includes(elem.name)) ? 
+                        container = container.filter(item => item !== elem.name) :
+                        container.push(elem.name);
+}
+
+})();
